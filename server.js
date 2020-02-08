4070bee3ce29df214ace0e2ccce8e9b44aa2674e
kkterai <kenlyn.terai@gmail.com>
2020-02-08 02:08:57 -0800

    Modify boilerplate server for successful db test

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
-//  Import the Express library
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require('./routes/api/users')
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const bodyparser = require('body-parser');
+const passport = require('passport');
 const app = express();
+
+//Body parser middleware
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+//Db Config
+const db = require("./config/keys").mongoURI;
+//Connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDb connected!"))
+  .catch(err => console.log(err));
+
+//Passport middleware
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+//First route
+app.get("/", (req, res) => res.send("Hello"));
+
+//Use routes
+// app.use('/api/users', users)
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5555;
+app.listen(port, () => console.log(`Server running on port ${port}`));
