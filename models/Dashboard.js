49ffec4efdb197144be3fe5af5f1b00cb1a8c40b
kkterai <kenlyn.terai@gmail.com>
2020-02-09 12:33:02 -0800

    Add Dashboard schema

---
+++ models/Dashboard.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const DashboardSchema = new Schema({
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    },
+    users: [
+        {
+          user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+          },
+          text: {
+            type: String,
+            required: true
+          },
+          name: {
+            type: String
+          },
+          avatar: {
+            type: String
+          },
+          date: {
+            type: Date,
+            default: Date.now
+          },
+          posts: [
+            {
+              user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+              },
+              text: {
+                type: String,
+                required: true
+              },
+              name: {
+                type: String
+              },
+              avatar: {
+                type: String
+              },
+              date: {
+                type: Date,
+                default: Date.now
+              }
+            }
+          ],
+        }
+      ],
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+module.exports = User = mongoose.model('users', DashboardSchema);
