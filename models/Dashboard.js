d4fcda23bd4caf2e90abe2e5ac47d2cde5ddbde3
kkterai <kenlyn.terai@gmail.com>
2020-02-23 02:48:29 -0800

    Revise Dashboard model to avoid redundant User model definition

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -2,58 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DashboardSchema = new Schema({
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
-    },
+    name: String,
     users: [
-        {
-          user: {
-            type: Schema.Types.ObjectId,
-            ref: 'users'
-          },
-          text: {
-            type: String,
-            required: true
-          },
-          name: {
-            type: String
-          },
-          avatar: {
-            type: String
-          },
-          date: {
-            type: Date,
-            default: Date.now
-          },
-          posts: [
-            {
-              user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-              },
-              text: {
-                type: String,
-                required: true
-              },
-              name: {
-                type: String
-              },
-              avatar: {
-                type: String
-              },
-              date: {
-                type: Date,
-                default: Date.now
-              }
-            }
-          ],
-        }
-      ],
-    date: {
-      type: Date,
-      default: Date.now
-    }
+      { 
+        type: Schema.Types.ObjectId, 
+        ref: 'user'
+      }
+    ]
   });
 
-module.exports = User = mongoose.model('users', DashboardSchema);
+module.exports = Dashboard = mongoose.model('dashboard', DashboardSchema);
