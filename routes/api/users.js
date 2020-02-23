04ef452ca0fa430b605f7408b12ada406dbdedea
kkterai <kenlyn.terai@gmail.com>
2020-02-23 04:20:00 -0800

    Implement bcrypt for password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // User model
 const User = require('../../models/User');
@@ -21,8 +22,8 @@ router.post("/register", (req, res) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (user) {
-          errors.email = "Email already exists";
-          return res.status(400).json(errors);
+            errors.email = "Email already exists";
+            return res.status(400).json(errors);
         } else {
           const avatar = gravatar.url(req.body.email, {
             s: "200",
@@ -30,17 +31,27 @@ router.post("/register", (req, res) => {
             d: "mm"
           });
   
-        new User({
+          const newUser = new User({
             name: req.body.name,
             email: req.body.email,
             avatar,
             password: req.body.password
-          })
-        .save()
-        .then(user => res.json(user))
-        .catch(err => console.log(err));;
-        }
-    })
+          });
+
+         bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+		})
+		.catch(err => console.log(err));
   });
 
 module.exports = router;
