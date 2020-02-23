e2e7cacdf43c627055df48ff18e43aa2f4009c79
kkterai <kenlyn.terai@gmail.com>
2020-02-23 03:34:40 -0800

    Create initial user registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require("gravatar");
 
 // User model
 const User = require('../../models/User');
@@ -11,5 +12,34 @@ router.get("/test", (req,res) =>
         msg: 'User api works!'
     })
 );
+
+// @route   POST api/users/register
+// @desc    Register user
+// @access  public
+router.post("/register", (req, res) => {
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (user) {
+          errors.email = "Email already exists";
+          return res.status(400).json(errors);
+        } else {
+          const avatar = gravatar.url(req.body.email, {
+            s: "200",
+            r: "pg",
+            d: "mm"
+          });
+  
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            avatar,
+            password: req.body.password
+          })
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));;
+        }
+    })
+  });
 
 module.exports = router;
