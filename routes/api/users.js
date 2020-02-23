0eb9bdc67bc38f888d69cf46504d715a7e437f83
kkterai <kenlyn.terai@gmail.com>
2020-02-23 04:25:44 -0800

    Implement login using JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys.js");
 
 // User model
 const User = require('../../models/User');
@@ -53,5 +55,44 @@ router.post("/register", (req, res) => {
 		})
 		.catch(err => console.log(err));
   });
+
+// @route   POST api/users/login
+// @desc    Login user / Returning JWT token
+// @access  public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: "User not found" });
+      }
+
+      //Check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          //create payload
+          const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+          //sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              return res.json({
+                token: "Bearer " + token
+              });
+            }
+          );
+        } else {
+          return res.status(400).json({ password: "Password incorrect!" });
+        }
+      });
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
