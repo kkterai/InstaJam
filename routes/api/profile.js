fde3756f181b4fd8e3d76e3d5b62133b94073713
kkterai <kenlyn.terai@gmail.com>
2020-02-23 23:00:50 -0800

    Add a POST route to create a user profile and implement profile validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,7 +5,66 @@ const passport = require('passport');
 // Profile model
 const Profile = require('../../models/Profile');
 
-// TODO: Validation
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    console.log(req.body)
+    const { errors, isValid } = validateProfileInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+    
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.postnumber) profileFields.postnumber = req.body.postnumber;
+    if (req.body.followernumber) profileFields.followernumber = req.body.followernumber;
+    if (req.body.followingnumber) profileFields.followingnumber = req.body.followingnumber;
+    if (req.body.name) profileFields.name = req.body.name;
+
+    // Posts - Spilt into array
+    if (typeof req.body.posts !== 'undefined') {
+      profileFields.posts = req.body.posts.split(',');
+    }
+    
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle })
+        .then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists';
+            res.status(400).json(errors);
+          }
+         // Save Profile
+         new Profile(profileFields)
+         .save()
+         .then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
 
 // @route   GET api/profile
 // @desc    Get current users profile
