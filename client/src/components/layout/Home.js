98bdb956fffd9d9375763c9550d0fc12ce48e5a0
kkterai <kenlyn.terai@gmail.com>
2020-03-24 12:54:27 -0700

    Add a Home component to display user feed

---
+++ client/src/components/layout/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            Feed of other users' Posts
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
