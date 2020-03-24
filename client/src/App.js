1b9bd8015e1fde3731a7bc6a9eb160c9a837c956
kkterai <kenlyn.terai@gmail.com>
2020-03-23 18:48:29 -0700

    Add Footer component to UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import SignUp from './components/auth/SignUp.js';
 import './index.css';
 
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Route exact path="/signup" component={SignUp} />
+        <Footer />
       </div>
     </Router>
   );
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="footer mt-5 p-4 text-center">
+      © 2020 INSTAJAM FROM <a className="footer" href='https://github.com/kkterai'>KKTERAI</a>
+    </footer>
+  );
+}
