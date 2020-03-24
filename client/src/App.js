ed3e6d208cdc4c88b04dbc6198eb1c333343ba45
kkterai <kenlyn.terai@gmail.com>
2020-03-24 09:13:55 -0700

    Minor format change for reader clarity - add space between style and component imports

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import SignUp from './components/auth/SignUp.js';
+
 import './index.css';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/signup" component={SignUp} />
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/signup" component={SignUp} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  
 }
 
 export default App;
