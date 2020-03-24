7ba3a83bd0347c3348ff042e861a73abf8b9f027
kkterai <kenlyn.terai@gmail.com>
2020-03-23 17:10:46 -0700

    Rename Header component to Navbar and move to layout directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from './components/layout/Navbar';
+import SignUp from './components/auth/SignUp.js';
 import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/signup" component={SignUp} />
+      </div>
+    </Router>
   );
 }
 
