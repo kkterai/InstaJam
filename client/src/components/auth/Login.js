3c57aec1cd5f3af82668e4943a4d52d9b714fc65
kkterai <kenlyn.terai@gmail.com>
2020-03-25 14:50:53 -0700

    Minor format changes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {withRouter} from 'react-router-dom';
+
 import axios from 'axios';
 import classnames from "classnames";
 import logo from '../../img/Instagram-text.png'
