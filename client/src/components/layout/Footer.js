4c8a087e908ef12018c7ada1479c5c3c4673dee7
kkterai <kenlyn.terai@gmail.com>
2020-03-25 13:50:05 -0700

    Refactor to utilize props in conditional rendering

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import isLoggedIn from '../../validation/is-logged-in'
 
-export default function Footer() {
+export default function Footer(props) {
   return (
-    !isLoggedIn(localStorage.jwtToken) ?
+    !isLoggedIn(props.token) ?
     <footer className="footer mt-5 p-4 text-center">
       © 2020 INSTAJAM FROM <a className="footer" href='https://github.com/kkterai'>KKTERAI</a>
     </footer>
     : null
-  ) 
+  )
 }
