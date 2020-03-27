ebb513594632adee488998651ef37d0cdac77ea7
kkterai <kenlyn.terai@gmail.com>
2020-03-26 20:21:48 -0700

    Add auth actions and auth reducer and error reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-  () => [], 
+  rootReducer, 
   initialState, 
   composeWithDevTools(applyMiddleware(...middleware))
     );
