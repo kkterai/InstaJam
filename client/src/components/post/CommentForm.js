a1431d7c8d0200f819dff8928e871d5eb779f446
kkterai <kenlyn.terai@gmail.com>
2020-04-07 00:14:32 -0700

    Rework comment form presentation and implement buttonless submission

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -22,9 +22,13 @@ class CommentForm extends Component {
     }
   }
 
-  onSubmit(e) {
-    e.preventDefault();
+  onKeyPress = (e) => {
+    if(e.which === 13) {
+      this.onSubmit();
+    }
+  }
 
+  onSubmit() {
     const { user } = this.props.auth;
     const { postId } = this.props;
 
@@ -47,29 +51,15 @@ class CommentForm extends Component {
     const { errors } = this.state;
 
     return (
-      <div className="post-form mb-3">
-        <div className="card card-info">
-          <div className="card-header bg-info text-white">
-            Make a comment...
-          </div>
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <TextAreaFieldGroup
-                  placeholder="Reply to post"
-                  name="text"
-                  value={this.state.text}
-                  onChange={this.onChange}
-                  error={errors.text}
-                />
-              </div>
-              <button type="submit" className="btn btn-dark">
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <form onKeyPress={this.onKeyPress}>
+        <TextAreaFieldGroup
+          placeholder="Add a comment..."
+          name="text"
+          value={this.state.text}
+          onChange={this.onChange}
+          error={errors.text}
+        />
+      </form>
     );
   }
 }
