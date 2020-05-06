bfa8ac2c421ba547c6fd4517bc00263576d73338
kkterai <kenlyn.terai@gmail.com>
2020-05-05 23:02:34 -0700

    Implement route to profile by username

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -26,11 +26,12 @@ router.post(
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (req.body.username) profileFields.username = req.body.username;
-    if (req.body.postnumber) profileFields.postnumber = req.body.postnumber;
-    if (req.body.followernumber) profileFields.followernumber = req.body.followernumber;
-    if (req.body.followingnumber) profileFields.followingnumber = req.body.followingnumber;
     if (req.body.name) profileFields.name = req.body.name;
+    if (req.body.username) profileFields.username = req.body.username;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.email) profileFields.email = req.body.email;
+    if (req.body.phone) profileFields.phone = req.body.phone;
+    if (req.body.website) profileFields.website = req.body.website;
 
     // Posts - Spilt into array
     if (typeof req.body.posts !== 'undefined') {
@@ -81,10 +82,31 @@ router.get('/',
           errors.noprofile = 'There is no profile for this user';
           return res.status(404).json(errors);
         }
+        console.log(profile)
         res.json(profile);
       })
       .catch(err => res.status(404).json(err));
   }
 )
+
+// @route   GET api/profile/username/:username
+// @desc    Get profile by username
+// @access  Public
+
+router.get('/username/:username', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ username: req.params.username })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
