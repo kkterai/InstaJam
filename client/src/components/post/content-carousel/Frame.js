1525811002d46a19807a992cebb7de5a97770177
kkterai <kenlyn.terai@gmail.com>
2020-06-12 21:53:22 -0700

    Minor readability edit

--- client/src/components/post/content-carousel/Frame.js
+++ client/src/components/post/content-carousel/Frame.js
@@ -3,5 +3,5 @@ import React from 'react';
 import styles from './frame-styles.js';
 
 export default function Frame({ children }) {
-  return <div style={styles.root}>{children}</div>
+  return <div style={styles.root}> { children } </div>
 }
