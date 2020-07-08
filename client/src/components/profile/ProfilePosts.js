d8ce1a0d341e67821900673d892ee30d0eda23b0
kkterai <kenlyn.terai@gmail.com>
2020-07-08 00:20:09 -0700

    Refactor code to create space for user posts in profile

---
+++ client/src/components/profile/ProfilePosts.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class ProfilePosts extends Component {
+  render() {
+    
+    return (
+      <div className="row">
+       
+      </div>
+    );
+  }
+}
+
+ProfilePosts.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+export default ProfilePosts
